9ec1d302594212b959fd004a33481441554c8d10
Kyle Sumagang <ksumagang2026@gmail.com>
2026-02-04 14:17:09 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by SUMAGANG, KYLE RUSSLIE on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
